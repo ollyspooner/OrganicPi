d64205a213d07a4efe9e5b64092939c59fa10ee0
Olly Spooner <OllySpooner@shelterbox.org>
2019-10-06 23:46:12 +0100

    Mostly working!

--- src/OrganicPi/OrganicPi.java
+++ src/OrganicPi/OrganicPi.java
@@ -2,85 +2,106 @@ package OrganicPi;
 
 import java.io.File;
 import java.io.IOException;
+import javax.sound.midi.*;
 
-import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiEvent;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.MidiUnavailableException;
-import javax.sound.midi.Sequence;
-import javax.sound.midi.Sequencer;
-import javax.sound.midi.ShortMessage;
-import javax.sound.midi.Track;
-
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalInput;
-import com.pi4j.io.gpio.PinPullResistance;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import com.pi4j.util.Console;
+import com.pi4j.util.ConsoleColor;
 
 public class OrganicPi {
-	public static void main( String[] args ) {
+	
+	private static int endOffsetSeconds = 10;
+	private static boolean playing = false;
+	
+	public static void main( String[] args ) throws InterruptedException {
+		final Console console = new Console();
+		console.title( "<-- OrganicPi -->", "MIDI organ player" );
+		console.promptForExit();
 		
 		// Listen for pin A or pin B
 		final GpioController gpio = GpioFactory.getInstance();
-		final GpioPinDigitalInput coinslotA = gpio.provisionDigitalInputPin( RaspiPin.GPIO_00, PinPullResistance.PULL_DOWN );
+		final GpioPinDigitalInput coinslotA = gpio.provisionDigitalInputPin( RaspiPin.GPIO_01, PinPullResistance.PULL_UP );
+		coinslotA.setDebounce( 1000 );
 		coinslotA.setShutdownOptions( true );
 		coinslotA.addListener( new GpioPinListenerDigital() {
 			@Override
 			public void handleGpioPinDigitalStateChangeEvent( GpioPinDigitalStateChangeEvent event ) {
-				if( event.getState() == PinState.HIGH ) {;
-					addperfomanceAtoplaylist();
+                console.println(" --> GPIO PIN STATE CHANGE (EVENT): " + event.getPin() + " = " +
+                        ConsoleColor.conditional(
+                                event.getState().isHigh(), // conditional expression
+                                ConsoleColor.GREEN,        // positive conditional colour
+                                ConsoleColor.RED,          // negative conditional colour
+                                event.getState()));        // text to display
+				if( event.getState() == PinState.LOW ) {
+					console.println( "A was pressed. Now playing " + args[0] );
+					playMidiFile( args[0] );
 				}
 			}
 		} );
-		
-		
-		
-		playMidiFile( args[0] );
-	}
-	
-	public static void addperfomanceAtoplaylist() {
-		System.out.println( "A was pressed." );
-	}
-
-	public void addperfomanceBtoplaylist() {
-		System.out.println( "B was pressed." );
+		final GpioPinDigitalInput coinslotB = gpio.provisionDigitalInputPin( RaspiPin.GPIO_04, PinPullResistance.PULL_UP );
+		coinslotB.setDebounce( 1000 );
+		coinslotB.setShutdownOptions( true );
+		coinslotB.addListener( new GpioPinListenerDigital() {
+			@Override
+			public void handleGpioPinDigitalStateChangeEvent( GpioPinDigitalStateChangeEvent event ) {
+                console.println(" --> GPIO PIN STATE CHANGE (EVENT): " + event.getPin() + " = " +
+                        ConsoleColor.conditional(
+                                event.getState().isHigh(), // conditional expression
+                                ConsoleColor.GREEN,        // positive conditional colour
+                                ConsoleColor.RED,          // negative conditional colour
+                                event.getState()));        // text to display
+				if( event.getState() == PinState.LOW ) {
+					console.println( "B was pressed. Now playing " + args[1] );
+					playMidiFile( args[1] );
+				}
+			}
+		} );
+		console.waitForExit();
+        gpio.shutdown();
 	}
 	
 	public static void playMidiFile ( String fileToPlay ) {
-		// Load a MIDI file and play it with something...
-		try {
-			Sequencer sequencer = MidiSystem.getSequencer();
-			if( sequencer == null ) {
-				System.err.println( "Sequencer device not supported" );
-				return;
+		if( !playing ) {
+			System.out.println( "Playing track from " + fileToPlay + "...");
+			// Load a MIDI file and play it with something...
+			try {
+				playing = true;
+				Sequencer sequencer = MidiSystem.getSequencer();
+				if( sequencer == null ) {
+					System.err.println( "Sequencer device not supported" );
+					return;
+				}
+				sequencer.open();
+				Sequence sequence = MidiSystem.getSequence( new File( fileToPlay ) );
+				Track track = sequence.createTrack();
+	
+				// Insert an unplayable note at the start of the sequence
+				track.add( makeEvent( ShortMessage.NOTE_ON, 0, 100, 64, 30 ) );
+				track.add( makeEvent( ShortMessage.NOTE_OFF, 0, 100, 64, 30 + 70 ) );
+				
+				// Find out how long the sequence is in seconds
+				long timeLength = sequence.getMicrosecondLength(); // 5900000 ms = 5900
+				long tickLength = sequence.getTickLength();
+				long msPerTick = timeLength / tickLength;
+				long startTheEnd = ( timeLength - ( endOffsetSeconds * 1000000 ) ) / msPerTick;
+	
+				// Insert an unplayable note {endOffsetSeconds} seconds before the end of the sequence
+				track.add( makeEvent( ShortMessage.NOTE_ON, 0, 105, 64, (int)startTheEnd ) );
+				track.add( makeEvent( ShortMessage.NOTE_OFF, 0, 105, 64, (int)startTheEnd + 70 ) );
+				
+				sequencer.setSequence( sequence );
+				sequencer.start();
+				while( sequencer.isRunning() ) {}
+				playing = false;
+				System.out.println( "The track finished playing!" );
+			} catch ( MidiUnavailableException | InvalidMidiDataException | IOException ex ) {
+				ex.printStackTrace();
+				
 			}
-			sequencer.open();
-			Sequence sequence = MidiSystem.getSequence( new File( fileToPlay ) );
-			Track track = sequence.createTrack();
-
-			// Insert an unplayable note at the start of the sequence
-			track.add( makeEvent( ShortMessage.NOTE_ON, 0, 100, 64, 30 ) );
-			track.add( makeEvent( ShortMessage.NOTE_OFF, 0, 100, 64, 30 + 70 ) );
-			
-			// Find out how long the sequence is in seconds
-			long timeLength = sequence.getMicrosecondLength(); // 5900000 ms = 5900
-			long tickLength = sequence.getTickLength();
-			long msPerTick = timeLength / tickLength;
-			long startTheEnd = ( timeLength - 5000000 ) / msPerTick;
-
-			// Insert an unplayable note 5 seconds before the end of the sequence
-			track.add( makeEvent( ShortMessage.NOTE_ON, 0, 105, 64, (int)startTheEnd  ) );
-			track.add( makeEvent( ShortMessage.NOTE_OFF, 0, 105, 64, (int)startTheEnd + 70 ) );
-			
-			sequencer.setSequence( sequence );
-			sequencer.start();
-		} catch ( MidiUnavailableException | InvalidMidiDataException | IOException ex ) {
-			ex.printStackTrace();
-			
+		} else {
+			System.out.println( "There is already a track playing! Wait for it to finish before inserting coins." );
 		}
 	}
 	
